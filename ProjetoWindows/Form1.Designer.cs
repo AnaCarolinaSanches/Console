5d3172b0651a9992e8d00ef5ff84c3cb6aca4194
ANA CAROLINA SANCHES <ana.sanches32@etec.sp.gov.br>
2021-02-10 22:37:45 -0300

    Meu Primeiro Commit

---
+++ ProjetoWindows/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace ProjetoWindows
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.cbJogador2 = new System.Windows.Forms.ComboBox();
+            this.cbJogador1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(351, 289);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "jogar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // cbJogador2
+            // 
+            this.cbJogador2.FormattingEnabled = true;
+            this.cbJogador2.Items.AddRange(new object[] {
+            "Pedra",
+            "Papel",
+            "Tesoura"});
+            this.cbJogador2.Location = new System.Drawing.Point(508, 90);
+            this.cbJogador2.Name = "cbJogador2";
+            this.cbJogador2.Size = new System.Drawing.Size(121, 21);
+            this.cbJogador2.TabIndex = 1;
+            // 
+            // cbJogador1
+            // 
+            this.cbJogador1.FormattingEnabled = true;
+            this.cbJogador1.Items.AddRange(new object[] {
+            "Pedra",
+            "Papel",
+            "Tesoura"});
+            this.cbJogador1.Location = new System.Drawing.Point(133, 90);
+            this.cbJogador1.Name = "cbJogador1";
+            this.cbJogador1.Size = new System.Drawing.Size(121, 21);
+            this.cbJogador1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(158, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 24);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Jogador1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(537, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Jogador2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cbJogador1);
+            this.Controls.Add(this.cbJogador2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox cbJogador2;
+        private System.Windows.Forms.ComboBox cbJogador1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ ProjetoWindows/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ProjetoWindows
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var jogador1 = cbJogador1.SelectedItem;
+            var jogador2 = cbJogador2.SelectedItem;
+
+            if (jogador1 == jogador2)
+            {
+                MessageBox.Show("Empatou");
+               
+            } else if (jogador1.ToString() == "Pedra" && jogador2.ToString() == "Tesoura")
+            {
+                MessageBox.Show("jogador1 Ganhou");
+            }          
+            else if (jogador1.ToString() =="Tesoura" && jogador2.ToString() == "Papel")
+            {
+                MessageBox.Show("Jogador1 Ganhou");
+
+            }
+            else if (jogador1.ToString() == "Papel" && jogador2.ToString() == "Pedra")
+            {
+                MessageBox.Show("Jogador1 Ganhou");
+            }
+            else
+            {
+                MessageBox.Show("Jogador 2 Ganhou");
+            }
+        }
+    }
+}
